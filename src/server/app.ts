42812f81301b89d16d061b79410e2ae286c399f1
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-08-04 14:59:12 +0100

    Add MongoDB connection

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,9 +1,23 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const url = process.env.MONGODB_URI;
+
+if (typeof url === "string") {
+  mongoose
+    .connect(url)
+    .then((_result) => {
+      console.log("connected to MongoDB");
+    })
+    .catch((error) => {
+      console.log("Error connecting to MongoDB: ", error.message);
+    });
+}
 
 export default app;
