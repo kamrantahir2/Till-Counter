c28880c7c73c09cec6506cfdcb29384e0b8d2d25
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2024-08-13 22:42:39 +0100

    Create fields for 1p and 2p, add submit button to form

--- src/components/TillTotal.tsx
+++ src/components/TillTotal.tsx
@@ -1,20 +1,47 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@radix-ui/react-label";
 import { useState } from "react";
+import { Button } from "./ui/button";
 
 const TillTotal = () => {
   const [oneP, setOneP] = useState(0);
+  const [twoP, setTwoP] = useState(0);
+
+  const handleSubmit = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    console.log(oneP);
+    console.log(twoP);
+  };
 
   return (
-    <div>
-      <div className="w-8/12">
-        <div className="grid w-3/12 items-start gap-1.5">
-          <Label className="mb-1" htmlFor="1p">
-            1p Coins
+    <div className="w-4/12">
+      <form onSubmit={handleSubmit}>
+        <div className="flex h-12 mb-4">
+          <Label className="w-24 leading-9" htmlFor="1p">
+            1p Coins:
           </Label>
-          <Input type="number" id="1p" placeholder="No. of 1p coins" />
+
+          <Input
+            onChange={(e) => setOneP(Number(e.target.value))}
+            type="number"
+            id="1p"
+            placeholder="No. of 1p coins"
+          />
         </div>
-      </div>
+        <div className="flex h-12 my-4">
+          <Label className="w-24 leading-9" htmlFor="2p">
+            2p Coins:
+          </Label>
+
+          <Input
+            onChange={(e) => setTwoP(Number(e.target.value))}
+            type="number"
+            id="2p"
+            placeholder="No. of 2p coins"
+          />
+        </div>
+        <Button type="submit">Submit</Button>
+      </form>
     </div>
   );
 };
