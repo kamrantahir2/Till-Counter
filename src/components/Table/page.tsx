b07c20452e726daa86e543c507bb80864c0f2d39
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2025-04-21 23:18:13 +0100

    Begin creating filtered context

--- src/components/Table/page.tsx
+++ src/components/Table/page.tsx
@@ -1,9 +1,16 @@
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
-import { useContext } from "react";
+import { useContext, createContext } from "react";
 import { TillContext } from "@/App";
+import { TillContextType } from "@/types";
+import { useState } from "react";
+import { PopulatedTill } from "@/types";
+
+export const FilteredContext = createContext<TillContextType | null>(null);
 
 export default function Table() {
+  const [filtered, setFiltered] = useState<PopulatedTill[]>([]);
+
   const tillContext = useContext(TillContext);
 
   const data = tillContext?.tills.reverse();
