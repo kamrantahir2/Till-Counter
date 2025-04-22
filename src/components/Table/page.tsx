c6f31ba4dd23540490d4d4310c26eb520cc764dc
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2025-04-22 19:44:54 +0100

    Fix bug where filtered state was lost on refreash

--- src/components/Table/page.tsx
+++ src/components/Table/page.tsx
@@ -1,6 +1,6 @@
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
-import { useContext, createContext } from "react";
+import { useContext, createContext, useEffect } from "react";
 import { TillContext } from "@/App";
 import { TillContextType } from "@/types";
 import { useState } from "react";
@@ -9,15 +9,21 @@ import { PopulatedTill } from "@/types";
 export const FilteredContext = createContext<TillContextType | null>(null);
 
 export default function Table() {
-  const [filtered, setFiltered] = useState<PopulatedTill[]>([]);
-
   const tillContext = useContext(TillContext);
 
   const data = tillContext?.tills.reverse();
 
+  const [filtered, setFiltered] = useState<PopulatedTill[]>(
+    tillContext!.tills!
+  );
+
+  useEffect(() => {
+    setFiltered(data!);
+  }, [tillContext?.tills]);
+
   return (
     <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={data!} />
+      <DataTable columns={columns} data={filtered!} />
     </div>
   );
 }
