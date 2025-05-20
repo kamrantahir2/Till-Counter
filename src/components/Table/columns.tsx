56144860adde785048050771b5d9cbbb10f5dfd6
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2025-05-20 15:46:12 +0100

    Add sorting for expected total

--- src/components/Table/columns.tsx
+++ src/components/Table/columns.tsx
@@ -66,7 +66,17 @@ export const columns: ColumnDef<PopulatedTill>[] = [
   },
   {
     accessorKey: "expectedTotal",
-    header: "Expected Total",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Expected Total
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
   },
   {
     accessorKey: "expectedVsTotal",
