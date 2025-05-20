b3ea1fc02e677b1bd23c13205674ebb764685f20
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2025-05-20 23:46:45 +0100

    Fix date sorting bug

--- src/components/Table/columns.tsx
+++ src/components/Table/columns.tsx
@@ -6,7 +6,7 @@ import { Button } from "../ui/button";
 export const columns: ColumnDef<PopulatedTill>[] = [
   {
     accessorKey: "date",
-    invertSorting: true,
+    invertSorting: false,
     header: ({ column }) => {
       return (
         <Button
