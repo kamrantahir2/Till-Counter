6c64a8fda380d378d1b1843f721e29af143a2c61
kamrantahir2 <59852029+kamrantahir2@users.noreply.github.com>
2025-05-27 21:14:24 +0100

    Remove unused imports git push

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,7 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { HashRouter, HashRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <HashRouter>
